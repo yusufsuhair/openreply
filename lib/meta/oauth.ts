b9d51e6a6fc2f60c59ba45a1dfd8701cf2745f0b
Yusuf Suhair <yusufmohdsuhair@gmail.com>
2026-08-22 18:00:12 +0800

    bugfix

--- lib/meta/oauth.ts
+++ lib/meta/oauth.ts
@@ -7,7 +7,7 @@ import {
 } from "crypto";
 import { getEncryptionKeyHex, requireEnv } from "@/lib/env";
 
-const INSTAGRAM_OAUTH_URL = "https://api.instagram.com/oauth/authorize";
+const INSTAGRAM_OAUTH_URL = "https://www.instagram.com/oauth/authorize";
 const INSTAGRAM_TOKEN_URL = "https://api.instagram.com/oauth/access_token";
 const ALGORITHM = "aes-256-gcm";
 const IV_LENGTH = 16;
